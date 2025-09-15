4eb9cc6261a68170160408be315255da69433a79
dy73033023 <dy73033023@tukorea.ac.kr>
2025-09-15 17:54:44 +0900

    2DGP-Drill02 2022184008 김성주

---
+++ 2DGP-Drill02.py
@@ -0,0 +1,71 @@
+from pico2d import *
+import math
+
+open_canvas()
+
+# fill here
+grass = load_image('grass.png')
+character= load_image('character.png')
+
+def rectangle_move():
+    x = 400
+    y = 90
+    while (x < 780):
+         clear_canvas_now()
+         grass.draw_now(400, 30)
+         character.draw_now(x,y)
+         x+=10
+         delay(0.01)
+
+    while (y < 560):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        y+=3
+        delay(0.01)
+
+    while (x > 20):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        x-=3
+        delay(0.01)
+
+    while (y > 90):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        y-=3
+        delay(0.01)
+
+    while (x < 400):
+         clear_canvas_now()
+         grass.draw_now(400, 30)
+         character.draw_now(x,y)
+         x+=3
+         delay(0.01)
+
+
+def circle_move():
+    cx, cy = 400, 330
+    r = 200
+    for degree in range(270, 270+360, 2):
+        theta = math.radians(degree)
+        x = cx + r * math.cos(theta)
+        y = cy + r * math.sin(theta)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        delay(0.01)
+
+while(1):
+    rectangle_move()
+    circle_move()
+
+close_canvas()
+
+
+
+
+
+
